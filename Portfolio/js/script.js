3fb01f110bcab6f7bb9e37f943877bc71de3f643
SebokaMokhele <74368106+SebokaMokhele@users.noreply.github.com>
2021-05-11 05:07:11 +0100

    portfolio
    portfolio

---
+++ Portfolio/js/script.js
@@ -0,0 +1,8 @@
+function openNav() {
+  document.getElementById("mySidenav-list").style.width = "350px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav-list").style.width = "0";
+}
+
